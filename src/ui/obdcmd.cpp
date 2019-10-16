0af1167f7623707511672700fbfc5bcaf17fe04f
Josh McIntyre <josh@jmcintyre.net>
2019-10-15 22:15:04 -0400

    Add the ability to dump one particular command item at a time. Also, refactor so that command information like Command::COMMAND enum, units, and labels are retrieved from a map

--- src/ui/obdcmd.cpp
+++ src/ui/obdcmd.cpp
@@ -56,6 +56,10 @@ void main_menu(ElmDevice &elm_device)
 		{
 			exit(0);
 		}
+		else if (cmd_items.find(menu_cmd) != cmd_items.end())
+		{
+			dump_item(elm_device, menu_cmd);
+		}
 		else
 		{
 			std::cout << "Invalid command. Enter 'help' for a list of valid commands" << std::endl;
@@ -63,34 +67,35 @@ void main_menu(ElmDevice &elm_device)
 	}
 }
 
+void dump_item(ElmDevice &elm_device, std::string item)
+{
+	std::cout << "Dumping requested OBDII data...\n";
+
+	std::string data = elm_device.get_data(cmd_items[item]);
+	std::cout << cmd_labels[item] << data << cmd_units[item] << std::endl;
+}
+
 void dump_all(ElmDevice &elm_device)
 {
-	std::cout << "Dumping all currently available OBDII data (this may take a moment)...\n";
-	
-	std::string data = elm_device.get_data(Command::GET_DTCS);
-	std::cout << "Diagnostic code(s): " << data << std::endl;
+	std::cout << "Dumping all currently available OBDII data...\n";
 
-	data = elm_device.get_data(Command::GET_COOLANT_TEMP);
-	std::cout << "Coolant temp: " << data << " \370C" << std::endl;
-	
-	data = elm_device.get_data(Command::GET_ENGINE_RPM);
-	std::cout << "Engine RPM: " << data << std::endl;
-	
-	data = elm_device.get_data(Command::GET_VEHICLE_SPEED);
-	std::cout << "Vehicle speed: " << data << " km/h" << std::endl;
-
-	data = elm_device.get_data(Command::GET_THROTTLE_POS);
-	std::cout << "Throttle position: " << data << "%" << std::endl;	
+	for (int i = 0; i < AVAILABLE_COMMANDS_SIZE; i++)
+	{
+		std::string item = available_items[i];
+		std::string data = elm_device.get_data(cmd_items[item]);
+		std::cout << cmd_labels[item] << data << cmd_units[item] << std::endl;
+	}
 }
 
 void show_help()
 {
 	std::cout << "'dumpall'\t\tDump all available OBDII data:\n";
-	std::cout << "\t\t\tFirst 3 diagnostic trouble code (DTCs)\n";
-	std::cout << "\t\t\tCurrent coolant temperature in degrees Celcius\n";
-	std::cout << "\t\t\tCurrent engine RPM\n";
-	std::cout << "\t\t\tCurrent vehicle speed in kilometers per hour\n";
-	std::cout << "\t\t\tCurrent throttle position as percentage of throttle used\n";
+	std::cout << "'<datapoint>'\t\tDump a specific item\n";
+	std::cout << "\t\t\t(dtc) : First 3 diagnostic trouble code (DTCs)\n";
+	std::cout << "\t\t\t(coo) : Current coolant temperature in degrees Celcius\n";
+	std::cout << "\t\t\t(rpm) : Current engine RPM\n";
+	std::cout << "\t\t\t(spd) : Current vehicle speed in kilometers per hour\n";
+	std::cout << "\t\t\t(thr) : Current throttle position as percentage of throttle used\n";
 	
 	std::cout << "'help'\t\t\tShow this help text\n";
 	
--- src/ui/obdcmd.h
+++ src/ui/obdcmd.h
@@ -8,5 +8,34 @@
 #include "elm_device.h"
 
 void main_menu(ElmDevice &elm_device);
+void dump_item(ElmDevice &elm_device, std::string item);
 void dump_all(ElmDevice &elm_device);
 void show_help();
+
+// Create maps of available command data to Command::COMMANDS, units, and labels
+const int AVAILABLE_COMMANDS_SIZE = 5;
+std::string available_items[AVAILABLE_COMMANDS_SIZE] = { "dtc", "coo", "rpm", "spd", "thr" };
+
+std::map<std::string, Command::COMMAND> cmd_items = {
+	{ "dtc", Command::GET_DTCS },
+	{ "coo", Command::GET_COOLANT_TEMP },
+	{ "rpm", Command::GET_ENGINE_RPM },
+	{ "spd", Command::GET_THROTTLE_POS },
+	{ "thr", Command::GET_THROTTLE_POS }
+};
+
+std::map<std::string, std::string> cmd_units = {
+	{ "dtc", "" },
+	{ "coo", "\370C" },
+	{ "rpm", " RPM" },
+	{ "spd", " km/h" },
+	{ "thr", "%" }
+};
+
+std::map<std::string, std::string> cmd_labels = {
+	{ "dtc", "Diagnostic code(s): " },
+	{ "coo", "Coolant temp: " },
+	{ "rpm", "Engine RPM: " },
+	{ "spd", "Vehicle speed: " },
+	{ "thr", "Throttle position: " }
+};
